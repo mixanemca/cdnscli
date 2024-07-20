2d260e98c0a6cd6a4f1f398fb796f27bea21d17d
Michael Bruskov <mixanemca@yandex.ru>
2024-07-20 19:13:42 +0400

    add app struct and interface

---
+++ app/app.go
@@ -0,0 +1,68 @@
+/*
+Copyright © 2024 Michael Bruskov <mixanemca@yandex.ru>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package app
+
+import (
+	"context"
+	"os"
+
+	"github.com/cloudflare/cloudflare-go"
+	"github.com/mixanemca/cfdnscli/internal/zones"
+)
+
+type app struct {
+	api   *cloudflare.API
+	zones zones.Client
+}
+
+// Option options for app
+type Option func(c *app) error
+
+// New creates a new CloudFlare API client. Various client options can be used to configure
+// the CloudFlare client
+func New(opts ...Option) (App, error) {
+	// App with default values
+	a := &app{}
+
+	for _, opt := range opts {
+		if err := opt(a); err != nil {
+			return nil, err
+		}
+	}
+
+	// TODO: get the token from config
+	api, err := cloudflare.NewWithAPIToken(os.Getenv("CLOUDFLARE_API_TOKEN"))
+	if err != nil {
+		return nil, err
+	}
+
+	// Verify token
+	_, err = api.VerifyAPIToken(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	a.api = api
+
+	a.zones = zones.New(a.api)
+
+	return a, nil
+}
+
+func (a *app) Zones() zones.Client {
+	return a.zones
+}
--- go.mod
+++ go.mod
@@ -9,7 +9,12 @@ require (
 )
 
 require (
+	github.com/cloudflare/cloudflare-go v0.100.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.7 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
@@ -26,8 +31,10 @@ require (
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/sys v0.18.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
