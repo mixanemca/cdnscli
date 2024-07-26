9b5a56ca1de2d19a948c94f1083b3030b0100940
Michael Bruskov <mixanemca@yandex.ru>
2024-07-26 16:03:00 +0400

    add rr delete subcommand

---
+++ cmd/rr-del.go
@@ -0,0 +1,83 @@
+/*
+Copyright © 2024 Michael Bruskov <mixanemca@yandex.ru>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/mixanemca/cfdnscli/app"
+	"github.com/spf13/cobra"
+)
+
+// rrDelCmd represents the add command
+var rrDelCmd = &cobra.Command{
+	Aliases: []string{"del", "rm", "remove", "unlink"},
+	Args:    cobra.NoArgs,
+	Use:     "delete",
+	Short:   "Delete resource record from zone",
+	Example: `  cfdnscli rr delete --name www --zone example.com --type A --content 192.0.2.1`,
+	Run:     rrDelCmdRun,
+}
+
+func init() {
+	rrCmd.AddCommand(rrDelCmd)
+
+	rrDelCmd.PersistentFlags().StringVarP(&content, "content", "c", "", "Comma separated IP address or domain name")
+	rrDelCmd.MarkPersistentFlagRequired("content")
+	rrDelCmd.PersistentFlags().StringVarP(&zone, "zone", "z", "", "Zone name")
+	rrDelCmd.MarkPersistentFlagRequired("zone")
+	rrDelCmd.PersistentFlags().StringVarP(&name, "name", "n", "", "Resource record name")
+	rrDelCmd.MarkPersistentFlagRequired("name")
+	rrDelCmd.PersistentFlags().StringVarP(&rrtype, "type", "t", "", "Type of the resource record (A, CNAME)")
+	rrDelCmd.MarkPersistentFlagRequired("type")
+}
+
+func rrDelCmdRun(cmd *cobra.Command, args []string) {
+	a, err := app.New()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// check that name not FQDN
+	if strings.Contains(name, zone) {
+		fmt.Printf("ERROR: Name (%s) must not be a FQDN. Without domain %s\n", name, zone)
+		os.Exit(1)
+	}
+
+	rrtype = strings.ToUpper(rrtype)
+
+	ctx, cancel := context.WithTimeout(context.Background(), clientTimeout)
+	defer cancel()
+
+	rr, err := a.Zones().GetRRByName(ctx, zone, name)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = a.Zones().DeleteRR(ctx, zone, rr)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("DNS resource record %s successfully deleted\n", rr.Name)
+}
