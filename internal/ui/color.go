e303259e4448041af01ed1383eac3f325cda7a8e
Michael Bruskov <mixanemca@yandex.ru>
2024-11-29 14:39:33 +0400

    move ui.Color to color.go file

---
+++ internal/ui/color.go
@@ -0,0 +1,37 @@
+/*
+Copyright © 2024 Michael Bruskov <mixanemca@yandex.ru>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ui
+
+import "github.com/charmbracelet/lipgloss"
+
+type Theme struct {
+	Primary   lipgloss.AdaptiveColor
+	Secondary lipgloss.AdaptiveColor
+	Highlight lipgloss.AdaptiveColor
+	Border    lipgloss.AdaptiveColor
+	Green     lipgloss.AdaptiveColor
+	Red       lipgloss.AdaptiveColor
+}
+
+var Color = Theme{
+	Primary:   lipgloss.AdaptiveColor{Light: "#000000", Dark: "#FFFFFF"},
+	Secondary: lipgloss.AdaptiveColor{Light: "#969B86", Dark: "#696969"},
+	Highlight: lipgloss.AdaptiveColor{Light: "#8b2def", Dark: "#8b2def"},
+	Border:    lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#383838"},
+	Green:     lipgloss.AdaptiveColor{Light: "#00FF00", Dark: "#00FF00"},
+	Red:       lipgloss.AdaptiveColor{Light: "#FF0000", Dark: "#FF0000"},
+}
--- internal/ui/model.go
+++ internal/ui/model.go
@@ -82,24 +82,6 @@ type Model struct {
 	ViewStyle  lipgloss.Style
 }
 
-type Theme struct {
-	Primary   lipgloss.AdaptiveColor
-	Secondary lipgloss.AdaptiveColor
-	Highlight lipgloss.AdaptiveColor
-	Border    lipgloss.AdaptiveColor
-	Green     lipgloss.AdaptiveColor
-	Red       lipgloss.AdaptiveColor
-}
-
-var Color = Theme{
-	Primary:   lipgloss.AdaptiveColor{Light: "#000000", Dark: "#FFFFFF"},
-	Secondary: lipgloss.AdaptiveColor{Light: "#969B86", Dark: "#696969"},
-	Highlight: lipgloss.AdaptiveColor{Light: "#8b2def", Dark: "#8b2def"},
-	Border:    lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#383838"},
-	Green:     lipgloss.AdaptiveColor{Light: "#00FF00", Dark: "#00FF00"},
-	Red:       lipgloss.AdaptiveColor{Light: "#FF0000", Dark: "#FF0000"},
-}
-
 func NewModel() *Model {
 	var m Model
 
